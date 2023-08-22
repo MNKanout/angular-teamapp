8a5a6400a26fda630fbd09015a417e74882caf98
Nour Kanout <nkanout@identecsolutions.com>
2023-08-22 11:19:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName ='';
+  members: string[] = [];
+  errorMessage = "";
+  numberOfTeams: number | ""  = "";
+
+  onInput(member: string){
+    this.newMemberName = member;
+  }
+  onNumberOfTeamsInput (value: string) {
+    this.numberOfTeams = Number(value);
+  }
+  addMember(){
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty"
+      return;
+    }
+      this.members.push(this.newMemberName);
+      this.newMemberName = "";
+      this.errorMessage = "";
+  }
+
 }
