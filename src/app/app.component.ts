43ad81dfaf309f199b73d802cd196ede06e2948d
Nour Kanout <nkanout@identecsolutions.com>
2023-08-22 14:06:48 +0200

    Implement the generate teams function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // States
   newMemberName ='';
   members: string[] = [];
   errorMessage = "";
   numberOfTeams: number | ""  = "";
+  teams: string[][] = []; // Array of string arrays.
 
   onInput(member: string){
     this.newMemberName = member;
@@ -26,5 +28,35 @@ export class AppComponent {
       this.newMemberName = "";
       this.errorMessage = "";
   }
+  generateTeams() {
+    const allMembers = [...this.members]
+    // Number of teams has to be positive number 
+    if(!this.numberOfTeams || this.numberOfTeams <= 0){
+      return
+    }
+    while (allMembers.length) {
+    // Create an array for each team.
 
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        // Generate random index to select a random element.
+        const randomIndex = Math.floor(Math.random()*allMembers.length)
+
+        // Return and remove the random array element. 
+        const member = allMembers.splice(randomIndex,1)[0] // Select the first element.
+
+        // Break if no members left.
+        if (!member) break;
+
+        // Check if the team exists in the teams array.
+        if (this.teams[i]) {
+          // Add the new member to the existing array.
+          this.teams[i].push(member)
+        } else {
+          // Create a new array, and add the member to it.
+          this.teams[i] = [member]
+        }
+      }
+    }
+    console.log(this.teams)
+  }
 }
